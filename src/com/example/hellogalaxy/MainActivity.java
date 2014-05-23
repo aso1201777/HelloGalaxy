a394816ca9040f486b5889b5aad78160c235a395
student <student@PC111S02.fgakusei.local>
2014-05-23 11:15:56 +0900

    ボタン処理追加

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener
 {
@@ -14,10 +17,29 @@ public class MainActivity extends Activity implements View.OnClickListener
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		//ボタンをＩＤで探してボタン変数を作る
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		//ボタン変数にリスナーを登録する。
+		btnOK.setOnClickListener(this);
+	}
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+			case R.id.btnOK:
+				//
+				EditText etv = (EditText)findViewById(R.id.edtFirst);
+					String inputMsg = etv.getText().toString();
+				EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+					String inputMsg2 = etv.getText().toString();
+					
+					TextView tv = (TextView)findViewById(R.id.txvMsg);
+					tv.setText("あなたでしたか、" + inputMsg + inputMsg2 + "さん。");
+		}
 
 	}
 
@@ -27,5 +49,7 @@ public class MainActivity extends Activity implements View.OnClickListener
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+
 
 }
